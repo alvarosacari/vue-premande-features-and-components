102f2482447a1381570b16d1332108512ba29cfe
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2020-09-21 00:34:14 -0500

    imports have been improved

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import router from '@/router/index.js'
+import store from '@/store/index.js'
+import App from '@/App.vue'
+import '@/registerServiceWorker.js'
 
 Vue.config.productionTip = false
 
