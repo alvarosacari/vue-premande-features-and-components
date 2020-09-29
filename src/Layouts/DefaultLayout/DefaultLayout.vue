7cd23afaa5a1a1b059b55b0581b7db5c58f52c64
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2020-09-29 00:02:08 -0500

    DefaultLayout component has been created

---
+++ src/Layouts/DefaultLayout/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <VuetifyLayout>
+    <slot />
+  </VuetifyLayout>
+</template>
+
+<script>
+import VuetifyLayout from '../VuetifyLayout/VuetifyLayout.vue'
+export default {
+  name: 'DefaultLayout',
+  components: {
+    VuetifyLayout
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
