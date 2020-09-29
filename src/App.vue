f8116131663b3f9c0cc2db64eed733a9e3b7bd41
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2020-09-29 00:02:36 -0500

    main App component has been updated

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,15 @@
 <template>
-  <v-app id="my-app">
-    <v-navigation-drawer
-      v-model="primaryDrawer.model"
-      :clipped="primaryDrawer.clipped"
-      :floating="primaryDrawer.floating"
-      :mini-variant="primaryDrawer.mini"
-      :permanent="primaryDrawer.type === 'permanent'"
-      :temporary="primaryDrawer.type === 'temporary'"
-      app
-      overflow
-    />
-
-    <v-app-bar
-      :clipped-left="primaryDrawer.clipped"
-      app
-    >
-      <v-app-bar-nav-icon
-        v-if="primaryDrawer.type !== 'permanent'"
-        @click.stop="primaryDrawer.model = !primaryDrawer.model"
-      />
-      <v-toolbar-title>VueJS</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-      <v-container fluid>
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col cols="10">
-            <v-card>
-              <v-card-text>
-                <v-row>
-                  <v-col
-                    cols="12"
-                    md="6"
-                  >
-                    <span>Scheme</span>
-                    <v-switch
-                      v-model="$vuetify.theme.dark"
-                      primary
-                      label="Dark"
-                    />
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="6"
-                  >
-                    <span>Drawer</span>
-                    <v-radio-group
-                      v-model="primaryDrawer.type"
-                      column
-                    >
-                      <v-radio
-                        v-for="drawer in drawers"
-                        :key="drawer"
-                        :label="drawer"
-                        :value="drawer.toLowerCase()"
-                        primary
-                      />
-                    </v-radio-group>
-                    <v-switch
-                      v-model="primaryDrawer.clipped"
-                      label="Clipped"
-                      primary
-                    />
-                    <v-switch
-                      v-model="primaryDrawer.floating"
-                      label="Floating"
-                      primary
-                    />
-                    <v-switch
-                      v-model="primaryDrawer.mini"
-                      label="Mini"
-                      primary
-                    />
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="6"
-                  >
-                    <span>Footer</span>
-                    <v-switch
-                      v-model="footer.inset"
-                      label="Inset"
-                      primary
-                    />
-                  </v-col>
-                </v-row>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-
-    <v-footer
-      :inset="footer.inset"
-      app
-    >
-      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+  <DefaultLayout>
+    <router-view />
+  </DefaultLayout>
 </template>
 
 <script>
+import DefaultLayout from './Layouts/DefaultLayout/DefaultLayout.vue'
+
 export default {
-  data () {
-    return {
-      drawers: ['Default', 'Permanent', 'Temporary'],
-      primaryDrawer: {
-        model: null,
-        type: 'default',
-        clipped: false,
-        floating: false,
-        mini: false
-      },
-      footer: {
-        inset: true
-      }
-    }
+  components: {
+    DefaultLayout
   }
 }
 </script>
